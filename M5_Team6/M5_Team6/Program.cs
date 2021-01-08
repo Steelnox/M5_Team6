d334cd1f2ce893fc02bfd250bbd4a6e9ce215fb3
Steelnox <eric14pastor@gmail.com>
2021-01-08 10:02:25 +0100

    Proyecto creado

---
+++ M5_Team6/M5_Team6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M5_Team6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
